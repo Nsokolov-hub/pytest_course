2607f4f6b2cfe0b9301f001ad54b9b6532245740
Николай Соколов <bug.hunter.nikolay@mail.ru>
2026-01-03 11:01:02 +0300

    изучаем пайтест_2

---
+++ tests/src/enums/global_enums.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class GlobalErrorssages(Enum):
+    WRONG_STATUS_CODE = "The response returned a wrong status code."
--- tests/test_something.py
+++ tests/test_something.py
@@ -1,9 +1,14 @@
 import requests
 from configuration import TESTS_URL
+from tests.src.enums.global_enums import GlobalErrorssages
+from jsonschema import validate
+from src.shemas.post import POST_SCHEMA
 
 def test_getting_posts():
     response = requests.get(TESTS_URL)
-    assert response.status_code == 200
+    assert response.status_code == 200, GlobalErrorssages.WRONG_STATUS_CODE.value
     posts = response.json()
     assert isinstance(posts, list)
     assert len(posts) > 0
+    
+    validate(recived_posts, POST_SCHEMA)
